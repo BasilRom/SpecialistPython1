92fe3483a7f22b2e514a6693ff0cedd8a50adb73
BasilRom <91020707+BasilRom@users.noreply.github.com>
2021-09-26 15:46:48 +0300

    Update 01_task_string.py

--- Module3/practice/string/01_task_string.py
+++ Module3/practice/string/01_task_string.py
@@ -6,5 +6,13 @@
 name = input("Имя: ")
 surname = input("Фамилия: ")
 
+mistake = name[0]
+mistake_true = mistake.upper()
+true_name = name.replace(mistake, mistake_true)
 
-print(name, surname)
+mistake_s = surname[0]
+mistake_true_s = mistake_s.upper()
+true_surname = surname.replace(mistake_s, mistake_true_s)
+
+print(true_name)
+print(true_surname)
