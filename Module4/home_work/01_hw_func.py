3e56a8a99a023d640ea9546166a721d78a7a347b
BasilRom <91020707+BasilRom@users.noreply.github.com>
2021-10-08 19:26:16 +0300

    Update 01_hw_func.py
    Программа работает, но если первая цифра (цифры) - "0", то, разумеется, выдаёт ошибку. Что делать?? Ведь в билетах номер может начинаться с нуля!

--- Module4/home_work/01_hw_func.py
+++ Module4/home_work/01_hw_func.py
@@ -2,9 +2,27 @@
 # Решение реализовать в виде функции.
 # Билет считается счастливым, если сумма его первых и последних цифр равны.
 
-def lucky_ticket(ticket_number):
-    # TODO: your code here
-    pass
+def lucky_tiket(n):
+
+    half1 = n//1000
+    half2 = n%1000
+
+    print(half1)
+    print(half2)
+
+    sum1 = half1//100 + half1%100//10 + half1%10
+    sum2 = half2//100 + half2%100//10 + half2%10
+    # print(sum1)       #проверка суммы 1
+    # print(sum2)       #проверка суммы 2
+    if sum2 == sum1:
+        return ("Вы счастливчик!")
+    else:
+        return ("Лузер!")
+
+
+
+print(lucky_ticket(301202))
+
 
 
 # Тестируем функцию
