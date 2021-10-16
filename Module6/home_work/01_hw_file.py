ff03e89701156c039cb684196240302abca44eec
BasilRom <91020707+BasilRom@users.noreply.github.com>
2021-10-16 15:08:14 +0300

    Update 01_hw_file.py
    Программа протестирована, работает. Брал примеры разных строк и разные файлы. "\n" - для более эстетичного восприятия текста на экране

--- Module6/home_work/01_hw_file.py
+++ Module6/home_work/01_hw_file.py
@@ -1,8 +1,15 @@
 # Напишите функцию log() принимающую в качестве аргумента строку и дописывающую это строку в конец файла
 
-def log(text, file="log.txt"):
-    pass
+# def log(text, file="log.txt"):
+#     pass
 
 
-log("hello world")  # дописывает "hello world" в конец файла log.txt
-log("message", "log01.txt")  # дописывает "message" в конец файла log01.txt
+# log("hello world")  # дописывает "hello world" в конец файла log.txt
+# log("message", "log01.txt")  # дописывает "message" в конец файла log01.txt
+
+def log(text, file):
+    f = open(file, 'a', encoding= 'utf-8')
+    f.write(text)
+    f.close()
+
+log('\nРагнар должен быть убит', "seek.txt")
